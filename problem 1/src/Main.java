c67d72d51cf109d3170f160b6f24e3e79a303c38
nealro <2111998@learn.vsb.bc.ca>
2023-09-21 12:37:54 -0700

    problem1

---
+++ problem 1/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int x = 113;
+        double y = 2.1828;
+        String name ="computer science";
+
+        System.out.println("100+13="+ x );
+        System.out.println("e can be rounded to " + y);
+        System.out.println("I am studing " + name);
+    }
+}
