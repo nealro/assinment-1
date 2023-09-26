1e6fbd90d367bdb81d88614f03275c3d11be52cc
nealro <2111998@learn.vsb.bc.ca>
2023-09-26 13:10:05 -0700

    problem 2

---
+++ problem2/src/Main.java
@@ -0,0 +1,34 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        String c1 = "|1|                            PEER TUTORING 12";
+        String c2 = "|2|                              LAW STUDIES 12";
+        String c3 = "|3|                     COMPUTER PROGRAMMING 12";
+        String c4 = "|4|                                  FREE BLOCK";
+        String c5 = "|5|                                CHEMISTRY 12";
+        String c6 = "|6|                                 CALCULUS 12";
+        String c7 = "|7|          CONTEMPORARY INDIGINEOUS STUDIES12";
+        String c8 = "|8|                                  PHYSICS 12";
+        String t1 = "|      Ms. Kim|";
+        String t2 = "|   Mr. Yetman|";
+        String t3 = "|  Mr. Zaremba|";
+        String t4 = "|       no one|";
+        String t5 = "|  Mr. Slapsys|";
+        String t6 = "|Mr. Grabowski|";
+        String t7 = "|Mr. Edmondson|";
+        String t8 = "|     Mr. Tang|";
+
+        System.out.println("+------------------------------------------------------------+");
+        System.out.println(c1 + t1);
+        System.out.println(c2 + t2);
+        System.out.println(c3 + t3);
+        System.out.println(c4 + t4);
+        System.out.println(c5 + t5);
+        System.out.println(c6 + t6);
+        System.out.println(c7 + t7);
+        System.out.println(c8 + t8);
+        System.out.println("+------------------------------------------------------------+");
+    }
+}
